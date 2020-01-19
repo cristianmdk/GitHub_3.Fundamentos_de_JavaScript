2914fe09326724252f25bb2851235ca66ff8b909
Cristian Angulo <cristianmdk@hotmail.com>
2020-01-19 20:16:35 +0700

    class1

---
+++ Class_1/clase1.js
@@ -0,0 +1,13 @@
+console.log('Hola soy Cristian JS')
+
+var nombre = 'Sacha', apellido = 'Angulo' // forma de declarar varias variables
+var edad = 28
+
+edad = '28 años'
+
+console.log('Hola ' + nombre + ' ' + apellido)
+console.log('Tengo ' + edad)
+
+/* Java es un lenguaje debilmente tipado porque 
+el tipo de las variables puede ser cambiado 
+a lo largo del programa */
