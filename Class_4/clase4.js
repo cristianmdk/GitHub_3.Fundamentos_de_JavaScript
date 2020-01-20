f853837ec8f2e775daaf9c02e25d2fa54d33e7b6
Cristian Angulo <cristianmdk@hotmail.com>
2020-01-20 12:24:16 +0700

    class5

--- Class_4/clase4.js
+++ Class_4/clase4.js
@@ -1,3 +1,21 @@
+function printage(names , ages){
+    var printvar = 'My name is ' + names + ' and I am ' + ages + ' years old'
+    console.log(printvar)
+}
+
+// USAR LA FUNCION
+var name = 'Cristian', age = 29
+printage(name, age)
+    //Java pa tratar de ejecuar porque es un lenguaje debilmente tipado
+    //Java es un lenguaje interpretado
+printage('name') 
+printage('Choice', 25)
+
+
+
+
+
+
 var nombre = 'William', edad = '34'
 
 function imprimirEdad(n, e){
