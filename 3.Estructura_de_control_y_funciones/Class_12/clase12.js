9849bfcecc636c9dbec4845e3ef121a817f22dde
Cristian Angulo <cristianmdk@hotmail.com>
2020-01-24 13:04:03 +0700

    class12

--- 3.Estructura_de_control_y_funciones/Class_12/clase12.js
+++ 3.Estructura_de_control_y_funciones/Class_12/clase12.js
@@ -13,26 +13,70 @@ var juan =  {
 const MAYOR_EDAD = 18
 
 // primera forma de declara las funciones
-/* function esMayorEdad(persona){
+function esMayorEdad1(persona){
     return persona.edad >= MAYOR_EDAD
-} */
+}
 
-// otra forma de declarar las funciones:
-/* const esMayorEdad = function (persona){
+// // otra forma de declarar las funciones:
+const esMayorEdad2 = function (persona){
     return persona.edad >= MAYOR_EDAD
-} */
+} 
 
-// también se puede declarar con flechas y las llaves para declar el cuerpo de la función:
-/* const esMayorEdad = persona => {
+// // también se puede declarar con flechas y las llaves para declar el cuerpo de la función:
+const esMayorEdad3 = persona => {
     return persona.edad >= MAYOR_EDAD
-} */
+}
 
-// otra forma de declar las funciones si y solo si retorna un valor:
-// const esMayorEdad = persona => persona.edad >= MAYOR_EDAD
+// // otra forma de declar las funciones si y solo si retorna un valor:
+const esMayorEdad4 = persona => persona.edad >= MAYOR_EDAD
 
-// otra forma es desestructurar el objeto, de la siguiente manera:
+// // otra forma es desestructurar el objeto, de la siguiente manera:
 const esMayorEdad = ({ edad }) => edad >= MAYOR_EDAD
-const esMenorEdad = ({edad}) => edad = !esMayorEdad({edad}) // se hace una negación con el signo de !
+
+
+
+
+
+
+function esMenorEdad1 (persona){
+    return persona.edad <= MAYOR_EDAD
+}
+
+function esMenorEdad2 ({edad}){
+    return edad <= MAYOR_EDAD
+}
+
+const esMenorEdad3 = function (persona){
+    return persona.edad <=MAYOR_EDAD
+}
+
+const esMenorEdad4 = function ({edad}){
+    return edad <= MAYOR_EDAD
+}
+
+const esMenorEdad5 = persona => {
+    return persona.edad <= MAYOR_EDAD
+}
+
+const esMenorEdad6 = ({edad}) => {
+    return edad <= MAYOR_EDAD
+}
+
+const esMenorEdad7 = persona => persona.edad <= MAYOR_EDAD
+
+const esMenorEdad8 = ({edad}) => edad <= MAYOR_EDAD
+
+const esMenorEdad9 = persona => !esMayorEdad(persona)
+
+const esMenorEdad10 = ({edad}) => !esMayorEdad({edad})
+
+const esMenorEdad = ({edad}) => edad = !esMayorEdad({edad}) 
+
+
+
+
+
+
 
 function validarMayoriaEdad(persona){
     
